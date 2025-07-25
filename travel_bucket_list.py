a1431e0ee39661e7f7eb7d21545a1e559586db13
Monisha <monishav2930@gmail.com>
2025-07-25 14:18:33 +0530

    Add Streamlit travel bucket list app

---
+++ travel_bucket_list.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title("My Travel Bucket List")
+
+st.header("Paris, France")
+st.image("https://upload.wikimedia.org/wikipedia/commons/e/e6/Paris_Night.jpg", caption="Eiffel Tower at night", width=400)
+st.write("I want to visit Paris to see the Eiffel Tower and enjoy French cuisine.")
+st.markdown("[View on Google Maps](https://goo.gl/maps/4Qp4rBqNYzG2)")
+
+st.header("Tokyo, Japan")
+st.image("https://upload.wikimedia.org/wikipedia/commons/1/16/Tokyo_Tower_and_surrounding_buildings.jpg", caption="Tokyo Tower", width=400)
+st.write("Tokyo for its unique culture, technology, and sushi!")
+st.markdown("[View on Google Maps](https://goo.gl/maps/Ggn3HXaH8MF2)")
